d31aef6838625fb406ab0ffdc00ff609a34efcf7
Joel Rose <mail@joel-rose.de>
2022-09-13 17:12:11 +0200

    feat: add api version & move routes to seperate folder

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"github.com/joelrose/crunch-merchant-service/api/channel/deliverect"
-	"github.com/joelrose/crunch-merchant-service/api/menus"
-	"github.com/joelrose/crunch-merchant-service/api/orders"
-	"github.com/joelrose/crunch-merchant-service/api/stores"
-	"github.com/joelrose/crunch-merchant-service/api/users"
-	"github.com/joelrose/crunch-merchant-service/api/whitelist"
-	"github.com/joelrose/crunch-merchant-service/auth_middleware"
 	"github.com/stripe/stripe-go/v73"
 
 	"github.com/joelrose/crunch-merchant-service/config"
@@ -22,44 +14,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func setupRoutes(e *echo.Echo, config config.Config) {
-	okHandler := func(c echo.Context) error {
-		return c.String(http.StatusOK, "ok")
-	}
-
-	e.GET("/", okHandler)
-
-	apiGroup := e.Group("/api")
-
-	apiGroup.GET("/whitelist", whitelist.IsWhitelisted)
-	apiGroup.GET("/stores", stores.GetStores)
-	apiGroup.GET("/menus/:id", menus.GetMenu)
-
-	channelGroup := apiGroup.Group("/channel")
-
-	deliverectGroup := channelGroup.Group("/deliverect")
-
-	deliverectGroup.POST("/channel_status", deliverect.DeliverectChannelStatus)
-	deliverectGroup.POST("/busy_mode", deliverect.DeliverectBusyMode)
-	deliverectGroup.POST("/menu_push", deliverect.DeliverectMenuPush)
-
-	dashboardGroup := apiGroup.Group("/dashboard", auth_middleware.Auth0Auth())
-
-	dashboardGroup.GET("/status", okHandler)
-
-	usersGroup := apiGroup.Group("/users", auth_middleware.FirebaseAuth(config.FirebaseConfig))
-
-	usersGroup.GET("/status", okHandler)
-
-	usersGroup.GET("/", users.GetUser)
-	usersGroup.POST("/", users.CreateUser)
-
-	ordersGroup := apiGroup.Group("/orders", auth_middleware.FirebaseAuth(config.FirebaseConfig))
-
-	ordersGroup.POST("/", orders.CreateOrder)
-}
-
-func dbMiddleware(db *db.DB) echo.MiddlewareFunc {
+func databaseMiddleware(db *db.DB) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			c.Set("db", db)
@@ -80,7 +35,7 @@ func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
-	e.Use(dbMiddleware(&db.DB{Sqlx: *database}))
+	e.Use(databaseMiddleware(&db.DB{Sqlx: *database}))
 
 	setupRoutes(e, c)
 
---
+++ routes.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/joelrose/crunch-merchant-service/api/v1/channel/deliverect"
+	"github.com/joelrose/crunch-merchant-service/api/v1/menus"
+	"github.com/joelrose/crunch-merchant-service/api/v1/orders"
+	"github.com/joelrose/crunch-merchant-service/api/v1/stores"
+	"github.com/joelrose/crunch-merchant-service/api/v1/users"
+	"github.com/joelrose/crunch-merchant-service/api/v1/whitelist"
+	"github.com/joelrose/crunch-merchant-service/auth_middleware"
+	"github.com/joelrose/crunch-merchant-service/config"
+	"github.com/labstack/echo/v4"
+)
+
+func setupRoutes(e *echo.Echo, config config.Config) {
+	okHandler := func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	}
+
+	e.GET("/", okHandler)
+
+	apiV1 := e.Group("/api/v1")
+
+	apiV1.GET("/whitelist", whitelist.IsWhitelisted)
+	apiV1.GET("/stores", stores.GetStores)
+	apiV1.GET("/menus/:id", menus.GetMenu)
+
+	channelGroup := apiV1.Group("/channel")
+	deliverectGroup := channelGroup.Group("/deliverect")
+
+	deliverectGroup.POST("/channel_status", deliverect.DeliverectChannelStatus)
+	deliverectGroup.POST("/busy_mode", deliverect.DeliverectBusyMode)
+	deliverectGroup.POST("/menu_push", deliverect.DeliverectMenuPush)
+
+	dashboardGroup := apiV1.Group("/dashboard", auth_middleware.Auth0Auth())
+
+	dashboardGroup.GET("/status", okHandler)
+
+	usersGroup := apiV1.Group("/users", auth_middleware.FirebaseAuth(config.FirebaseConfig))
+
+	usersGroup.GET("/status", okHandler)
+
+	usersGroup.GET("/", users.GetUser)
+	usersGroup.POST("/", users.CreateUser)
+
+	ordersGroup := apiV1.Group("/orders", auth_middleware.FirebaseAuth(config.FirebaseConfig))
+
+	ordersGroup.POST("/", orders.CreateOrder)
+}
