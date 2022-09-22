a6abf14e41eb02efb8a662e4a8f3b858114fa14d
Joel Rose <mail@joel-rose.de>
2022-09-22 18:41:46 +0200

    feat(wip): hauke

--- api/v1/dashboard/get_menu.go
+++ api/v1/dashboard/get_menu.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-redis/redis/v9"
+	"github.com/google/uuid"
 	"github.com/joelrose/crunch-merchant-service/db"
 	"github.com/joelrose/crunch-merchant-service/domain/menus"
 	"github.com/joelrose/crunch-merchant-service/middleware"
@@ -24,18 +25,18 @@ import (
 func GetMenu(c echo.Context) error {
 	db := c.Get(middleware.DATBASE_CONTEXT_KEY).(*db.DB)
 
-	userId := c.Get(middleware.AUTH0_USER_ID_CONTEXT_KEY)
+	/*userId := c.Get(middleware.AUTH0_USER_ID_CONTEXT_KEY)
 	userIdString := userId.(string)
 
 	storeId, err := db.GetStoreByMerchantUserId(userIdString)
 	if err != nil {
 		log.Errorf("Failed to get store by merchant user id: %v", err)
 		return echo.NewHTTPError(http.StatusForbidden)
-	}
+	}*/
 
 	rdb := c.Get(middleware.REDIS_CONTEXT_KEY).(*redis.Client)
 
-	menuService := menus.NewMenuService(db, rdb, storeId)
+	menuService := menus.NewMenuService(db, rdb, uuid.MustParse("69db0f24-7d29-4b95-9483-d3e1ab661c65"))
 	menu, err := menuService.GetMenu()
 	if err != nil {
 		log.Debugf("failed to get menu: %v", err)
